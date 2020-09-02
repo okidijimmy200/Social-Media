9d08898c18a09ffef6ae713748c23303b5cd95ab
okidijimmy200 <okidijimmie@gmail.com>
2020-09-02 11:43:38 +0300

    Development dependencies

---
+++ config/config.js
@@ -0,0 +1,19 @@
+const config = {
+    //env: To differentiate between development and production modes
+    env: process.env.NODE_ENV || "development",
+    //port: To define the listening port for the server
+    port: process.env.PORT || 3000,
+    //jwtSecret: The secret key to be used to sign JWT
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    //mongoUri: The location of the MongoDB database instance for the project
+    mongoUri: process.env.MONGODB_URI || 
+    process.env.MONGODB_HOST || 
+    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+    (process.env.MONGODB_HOST || '27017') +
+    '/mernproject'
+}
+
+export default config
+
+// These variables will give us the flexibility to change values from a single file and use
+// it across the backend code
---
+++ server/express.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import helmet from 'helmet'
+import compression from 'compression'
+
+const app = express()
+
+/* ...configure express */
+/**body-parser: Request body-parsing middleware to handle the
+complexities of parsing streamable request objects so that we can simplify
+browser-server communication by exchanging JSON in the request body. */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(cookieParser()) //Cookie parsing middleware to parse and set cookies in request objects.
+app.use(compression()) //Compression middleware that will attempt to compress response bodies for all requests that traverse through the middleware.
+app.use(helmet())  //Collection of middleware functions to help secure Express apps by setting various HTTP headers
+app.use(cors()) //Middleware to enable cross-origin resource sharing (CORS)
+
+
+export default app
