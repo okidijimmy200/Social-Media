0498a54ca4b89cd306a6f759484f9b4da92ed23c
okidijimmy200 <okidijimmie@gmail.com>
2020-09-04 11:26:07 +0300

    Mongoose error handling

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     //env: To differentiate between development and production modes
     env: process.env.NODE_ENV || "development",
     //port: To define the listening port for the server
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     //jwtSecret: The secret key to be used to sign JWT
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     //mongoUri: The location of the MongoDB database instance for the project
