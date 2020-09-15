21bdc06c45a8c9aa53dcebcfa737f6e10e9e6808
okidijimmy200 <okidijimmie@gmail.com>
2020-09-15 11:39:24 +0300

    completed the delteUser component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -5,6 +5,7 @@ import Users from './users/Users'
 import Signup from './users/Signup'
 import Signin from './auth/Signin'
 import Profile from './users/Profile'
+import PrivateRoute from './auth/PrivateRoute'
 
 
 const MainRouter = () => {
@@ -15,6 +16,9 @@ const MainRouter = () => {
                 <Route path="/users" component={Users} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/signin" component={Signin} />
+{/* we will use a PrivateRoute, which
+will restrict the component from loading at all if the user is not signed in. */}
+                <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
                 <Route path="/user/:userId" component={Profile} />
             </Switch>
         </div>
